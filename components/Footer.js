5832d5409c2f7d03c7ac3663dc6f4fa2aca4e3d5
Alithepronce <gamegdeo@gmail.com>
2026-06-04 01:40:41 +0300

    Add Footer component

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <div className="footer-logo">مكتب الموسوي © {new Date().getFullYear()}</div>
+      <p>حلول برمجية ذكية وآمنة تناسب تطلعاتكم.</p>
+    </footer>
+  );
+}
